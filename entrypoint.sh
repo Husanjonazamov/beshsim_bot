10445cd4cd233a005a8e7b75760189a67129e7da
Husanboy Azamov <azamovhusanboy08@gmail.com>
2024-12-09 18:33:35 +0500

    change

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
 set -e
+
+# Django migratsiyalarini bajarish
 python3 manage.py migrate
+
+# Django serverini ishga tushurish
 python3 manage.py runserver 0.0.0.0:8000 &
 
+# Botni ishga tushurish
 python3 bot.py
+
+# Skriptni to'liq bajarish uchun
+wait $!
